af5a4db5997c77331ad434178afc78e1d8929f79
Ihor Barban <ihorbarban@Ihors-MacBook-Air.local>
2025-03-01 17:48:58 +0100

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { useEffect, useState } from 'react';
+import { getTodos } from './api';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from './app/store';
+import { setTodos } from './features/todos';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App = () => {
+  const dispatch = useDispatch();
+  const todosFromServer = useSelector((state: RootState) => state.todos);
+  const currentTodo = useSelector((state: RootState) => state.currentTodo);
+  const [isLoading, setIsLoading] = useState(false);
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  useEffect(() => {
+    setIsLoading(true);
+    getTodos()
+      .then(resp => dispatch(setTodos(resp)))
+      .finally(() => setIsLoading(false));
+  }, [dispatch]);
 
-          <div className="block">
-            {false && <Loader />}
-            <TodoList />
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter />
+            </div>
+
+            <div className="block">
+              {isLoading ? <Loader /> : <TodoList todos={todosFromServer} />}
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    {false && <TodoModal />}
-  </>
-);
+      {currentTodo && <TodoModal />}
+    </>
+  );
+};
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import filterReducer from '../features/filter';
+import todosReducer from '../features/todos';
+import currentTodoReducer from '../features/currentTodo';
 
 const rootReducer = combineReducers({
   filter: filterReducer,
+  todos: todosReducer,
+  currentTodo: currentTodoReducer,
 });
 
 export const store = configureStore({
